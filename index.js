10dcdf84e5868ae5b41ede1a15621c3790dc9e8e
Joseph bayad <jobayad66@gmail.com>
2021-02-15 13:12:30 +0100

    add nodemon and watch the server

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('<h1>hello world</h1>');
+  res.send('<h1>hello Expresss</h1>');
 });
 
 const port = process.env.PORT || 5000;
