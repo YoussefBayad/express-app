3338f0e492877b2e2e1ebf1b571e9b909194bd92
Joseph bayad <jobayad66@gmail.com>
2021-02-15 13:35:04 +0100

    creating my own middleware

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const jsonData = ['its a jason data'];
 
+const myMiddleware = (req, res, next) => {
+  console.log(`${req.protocol}://${req.get('host')}://${req.originalUrl}`);
+};
+app.use(myMiddleware);
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.get('/api/jsondata', (req, res) => {
+  res.json(jsonData);
+});
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`server is running on port ${port}`));
